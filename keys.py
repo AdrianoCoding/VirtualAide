c3bdfe51627205bf695ab3af4f05953adab329a7
Xituis <70298185+Xituis@users.noreply.github.com>
2023-05-06 19:35:10 -0300

    Chaves necessaria para o script funcionar

--- keys.py
+++ keys.py
@@ -4,5 +4,5 @@ openai.api_key = 'YOUR OPENAI KEY' #Use a key do seu openai api
 admin = [YOUR ID TELEGRAM] # O id da sua conta no telegram
 token = 'YOUR BOT TOKEN_TELEGRAM' # O ID do seu bot
 bot = telebot.TeleBot(token)
-comp = ' ,responda apenas em português brasileiro, e não me envie links ou script' #Para evitar bugs de envio de script do nada
+comp = ' ,responda apenas em português brasileiro, e não me envie links ou script' #Para evitar bugs de envio de script do nada, altere dependendo da sua necessidade, é com está variavel que você irá controlar a personalidade do bot
 
